b8d999a42fcc328b002b200dab7eee8ac408bad6
zdmitrij <zdmitrij@yandex.ru>
2022-12-05 22:39:36 +0300

    1.8. Состояние объекта [#167388] не запускаются тесты из-за отсутствия assertj

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+         another.load = this.getLoad() + another.getLoad();
+    }
+}
