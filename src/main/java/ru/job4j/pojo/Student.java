8b88b0166ed21d1f8b51d4f349f3dcac81a36492
zdmitrij <zdmitrij@yandex.ru>
2022-12-15 21:17:45 +0300

    2. Модель данных. [#174683] не работает

---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,32 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+public class Student {
+    private String name;
+    private int group;
+    private Date entrance;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getGroup() {
+        return group;
+    }
+
+    public Date getEntrance() {
+        return entrance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+
+    public void setEntrance() {
+        this.entrance = entrance;
+    }
+}
