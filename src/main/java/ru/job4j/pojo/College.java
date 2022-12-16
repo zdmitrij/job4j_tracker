e6ecb6d7202a8185ae6ce8a264b20354cf7a42d4
zdmitrij <zdmitrij@yandex.ru>
2022-12-16 11:42:11 +0300

    3. Массивы и модели. [#174892]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -1,13 +1,13 @@
-package ru.job4j.pojo;
+//package ru.job4j.pojo;
 
-import java.util.Date;
+//import java.util.Date;
 
-public class College {
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("Nikolay");
-        student.setGroup(5);
-        student.setEntrance(new Date());
-        System.out.println(student.getName() + student.getGroup() + student.getEntrance());
-    }
-}
+//public class College {
+  ///  public static void main(String[] args) {
+     //   Student student = new Student();
+       // student.setName("Nikolay");
+        //student.setGroup(5);
+        //student.setEntrance(new Date());
+        //System.out.println(student.getName() + student.getGroup() + student.getEntrance());
+    //}
+//}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book dostoevsky = new Book("Idiot", 500);
+        Book barto = new Book("Stihi", 200);
+        Book krylov = new Book("Basni", 450);
+        Book martin = new Book("Clean code", 460);
+        Book[] books = new Book[4];
+        books[0] = dostoevsky;
+        books[1] = barto;
+        books[2] = krylov;
+        books[3] = martin;
+        for (int index = 0; index < books.length; index++) {
+            System.out.println("Book name is " + books[index].getName()
+                    + " and it contain " + books[index].getPage() + " pages");
+        }
+        System.out.println();
+        System.out.println("0 and 3 indexes are changed");
+        Book empty;
+        empty = books[0];
+        books[0] = books[3];
+        books[3] = empty;
+        for (int index = 0; index < books.length; index++) {
+            System.out.println("\tBook name is " + books[index].getName()
+                    + " and it contain " + books[index].getPage() + " pages");
+        }
+        System.out.println();
+        System.out.println("Selected only \"Clean code\" ");
+        for (int index = 0; index < books.length; index++) {
+            if (books[index].getName() == "Clean code") {
+                System.out.println("\tClean code is book with index " + index + " and  it contain "
+                        + books[index].getPage() + " pages");
+            }
+        }
+    }
+}
