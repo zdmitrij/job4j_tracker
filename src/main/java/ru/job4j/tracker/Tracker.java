3ebdf057847d0279a559123d746a2a3ab4ee10ac
zdmitrij <zdmitrij@yandex.ru>
2022-12-21 23:30:00 +0300

    6. Метод замены заявки. Tracker.replace [#211748]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -50,4 +50,25 @@ public class Tracker {
         }
         return rsl;
     }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+    int index = indexOf(id);
+    item.setId(id);
+    if (index != -1) {
+        items[index] = item;
+        return true;
+    }
+    return false;
+    }
 }
