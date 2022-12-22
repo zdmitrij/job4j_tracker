e9802cf1e3e99c72a6d894c0aca5e97d6275dbe3
zdmitrij <zdmitrij@yandex.ru>
2022-12-22 23:36:40 +0300

    5. Tracker - хранилище [#396 #373436] исправлено findAll убрал комментарии

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,14 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        //size = 0;
         Item[] rsl = new Item[items.length];
-        //for (int index = 0; index < items.length; index++) {
-            //if (items[index] != null) {
-                //rsl[size] = items[index];
-                //size++;
-            //}
-        //}
         return Arrays.copyOf(items, size);
         }
 
