909ec1a06b4ffbc69eec151dbe1d9386f66a2bf2
zdmitrij <zdmitrij@yandex.ru>
2022-12-25 09:48:30 +0300

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -7,5 +7,6 @@ public class StartUI {
         Item itemObject = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         System.out.println(itemObject.getCreated().format(formatter));
+        System.out.println(itemObject);
     }
 }
