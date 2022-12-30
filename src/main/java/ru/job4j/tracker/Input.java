9c1ae25afba7226045ebc5633c4b7cd68a571d6e
zdmitrij <zdmitrij@yandex.ru>
2022-12-30 20:39:26 +0300

    7. Статические методы. [#181779]

--- src/main/java/ru/job4j/tracker/Input.java
+++ src/main/java/ru/job4j/tracker/Input.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public interface Input {
+public  interface Input {
     String askStr(String question);
 
     int askInt(String question);
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -3,9 +3,10 @@ package ru.job4j.tracker;
 import java.util.Arrays;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    @SuppressWarnings("checkstyle:ConstantName")
+    private  final Item[] items = new Item[100];
     private int ids = 1;
-    private int size = 0;
+    private  int size = 0;
 
     public Item add(Item item) {
         item.setId(ids++);
@@ -13,7 +14,7 @@ public class Tracker {
         return item;
     }
 
-    public Item[] findAll() {
+    public  Item[] findAll() {
         return Arrays.copyOf(items, size);
     }
 
