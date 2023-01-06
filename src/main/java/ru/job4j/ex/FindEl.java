c63f27c227cce3e6dee114c60126f3131f2f6f20
zdmitrij <zdmitrij@yandex.ru>
2023-01-06 21:45:05 +0300

    0.4. Пользовательские исключения. [#219367] добавил throws ElementNotFoundException в main метод

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -14,7 +14,7 @@ public class FindEl {
         return rsl;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ElementNotFoundException {
         String[] value = {"One", "Two", "Three"};
         try {
             indexOf(value, "Four");
