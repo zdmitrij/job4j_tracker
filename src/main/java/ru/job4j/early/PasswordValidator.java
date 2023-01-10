dd7edcd6d2de130f2f071b3010be97156ff69fad
zdmitrij <zdmitrij@yandex.ru>
2023-01-10 14:47:52 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -5,8 +5,7 @@ import static java.lang.Character.*;
 public class PasswordValidator {
 
     public static String validate(String password) {
-
-        if (password.equals(null)) {
+        if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
@@ -14,64 +13,58 @@ public class PasswordValidator {
         }
         if (isNoUpperCase(password)) {
             throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-            }
+        }
         if (isNoLowerCase(password)) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-            }
+        }
         if (isNoDigit(password)) {
             throw new IllegalArgumentException("Password should contain at least one figure");
         }
-
-        if (toLowerCase(password).equals("qwerty") || toLowerCase(password).equals("qwerty") ||) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
+        if (isNoSpecialSymbol(password)) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        if (password.toLowerCase().equals("qwerty") || password.toLowerCase().equals("password")
+                || password.toLowerCase().equals("admin") || password.toLowerCase().equals("user")
+                || password.equals("12345")) {
+            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
 
         }
-        }
-
+        return password;
+    }
 
     public static boolean isNoUpperCase(String password) {
-
-    for (int i = 0; i < password.length(); i++) {
-           if(isUpperCase(password.codePointAt(i))) {
+        for (int i = 0; i < password.length(); i++) {
+            if (isUpperCase(password.codePointAt(i))) {
                 return false;
             }
-            return true;
         }
+        return true;
     }
 
     public static boolean isNoLowerCase(String password) {
         for (int i = 0; i < password.length(); i++) {
-            if(isLowerCase(password.codePointAt(i))) {
+            if (isLowerCase(password.codePointAt(i))) {
                 return false;
             }
-            return true;
         }
+        return true;
     }
 
     public static boolean isNoDigit(String password) {
         for (int i = 0; i < password.length(); i++) {
-            if(isDigit(password.codePointAt(i))) {
+            if (isDigit(password.codePointAt(i))) {
                 return false;
             }
-            return true;
         }
+        return true;
     }
 
-    public  static boolean isNoSpecialSymbol(String password) {
+    public static boolean isNoSpecialSymbol(String password) {
         for (int i = 0; i < password.length(); i++) {
-            if(password.codePointAt(i) = 36 || password.codePointAt(i) = 95) {
+            if (!isUpperCase(password.codePointAt(i)) && !isLowerCase(password.codePointAt(i)) && !isDigit(password.codePointAt(i))) {
                 return false;
             }
-            return true;
-
         }
+        return true;
     }
-
-    public static String lowerCase(String Password) {
-
-
-    }
-
-
-
 }
---
+++ src/main/java/ru/job4j/ex/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package ru.job4j.ex;
+
+public class IllegalArgumentException extends Exception {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
