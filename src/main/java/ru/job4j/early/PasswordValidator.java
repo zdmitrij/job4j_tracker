b2c83c356f22acf914d1c985805fce710cd14b99
zdmitrij <zdmitrij@yandex.ru>
2023-01-11 21:24:20 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #379564] 4 цикла заменено на 1, введен массив недопустимых паролей, использован contain вместо equals

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -4,67 +4,67 @@ import static java.lang.Character.*;
 
 public class PasswordValidator {
 
-    public static String validate(String password) {
+    public static String validate(String password, String message) {
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (isNoUpperCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        if (noSymbolsOrDigitsOrSpecs(password)) {
+            throw new IllegalArgumentException(message);
         }
-        if (isNoLowerCase(password)) {
-            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-        }
-        if (isNoDigit(password)) {
-            throw new IllegalArgumentException("Password should contain at least one figure");
-        }
-        if (isNoSpecialSymbol(password)) {
-            throw new IllegalArgumentException("Password should contain at least one special symbol");
-        }
-        if (password.toLowerCase().equals("qwerty") || password.toLowerCase().equals("password")
-                || password.toLowerCase().equals("admin") || password.toLowerCase().equals("user")
-                || password.equals("12345")) {
+        if (passwordContainThisWords(password)) {
             throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
 
         }
         return password;
     }
 
-    public static boolean isNoUpperCase(String password) {
+    public static boolean isSpecialCase(int i) {
+        if (!isUpperCase(i) && !isLowerCase(i) && !isDigit(i)) {
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean passwordContainThisWords(String password) {
+        boolean rsl = false;
+        String[] arrayOfWords = {"qwerty", "password", "admin", "user", "12345"};
+        for (int i = 0; i < arrayOfWords.length; i++) {
+            if (password.toLowerCase().contains(arrayOfWords[i])) {
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean noSymbolsOrDigitsOrSpecs(String password) {
+        final  String message;
+        boolean noUpperCase = true;
+        boolean noLowerCase = true;
+        boolean noDigit = true;
+        boolean noSpecialCase = true;
         for (int i = 0; i < password.length(); i++) {
             if (isUpperCase(password.codePointAt(i))) {
-                return false;
+                noUpperCase = false;
+                message = "Password should contain at least one uppercase letter";
             }
-        }
-        return true;
-    }
-
-    public static boolean isNoLowerCase(String password) {
-        for (int i = 0; i < password.length(); i++) {
             if (isLowerCase(password.codePointAt(i))) {
-                return false;
+                noLowerCase = false;
+                message = "Password should contain at least one lowercase letter";
             }
-        }
-        return true;
-    }
-
-    public static boolean isNoDigit(String password) {
-        for (int i = 0; i < password.length(); i++) {
             if (isDigit(password.codePointAt(i))) {
-                return false;
+                noDigit = false;
+                message = "Password should contain at least one figure";
+            }
+            if (isSpecialCase(password.codePointAt(i))) {
+                noSpecialCase = false;
+                message = "Password should contain at least one special symbol";
             }
         }
-        return true;
-    }
 
-    public static boolean isNoSpecialSymbol(String password) {
-        for (int i = 0; i < password.length(); i++) {
-            if (!isUpperCase(password.codePointAt(i)) && !isLowerCase(password.codePointAt(i)) && !isDigit(password.codePointAt(i))) {
-                return false;
-            }
-        }
-        return true;
+        boolean rsl = (noUpperCase || noLowerCase || noDigit || noSpecialCase);
+        return rsl;
     }
 }
